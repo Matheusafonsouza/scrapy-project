81994741c65dbc9c2b342b172a0624383a4ae7e9
matheusafonsouza <matheusafonsouza@gmail.com>
2021-11-23 00:24:50 -0300

    feat: quote pipeline saving on sqlite

--- quotes/project/pipelines.py
+++ quotes/project/pipelines.py
@@ -1,13 +1,31 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
+import sqlite3
 from itemadapter import ItemAdapter
 
 
-class ProjectPipeline:
+class QuotePipeline:
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+
+    def create_connection(self):
+        self.connection = sqlite3.connect('quotes.db')
+        self.cursor = self.connection.cursor()
+
+    def create_table(self):
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS QUOTE(
+                                title TEXT,
+                                author TEXT,
+                                tags text)
+                            """)
+
+    def store_db(self, item):
+        self.cursor.execute("""INSERT INTO QUOTE (title, author, tags) VALUES (?, ?, ?)""", (
+            item['title'],
+            item['author'],
+            ', '.join(item['tags'])
+        ))
+
     def process_item(self, item, spider):
+        self.store_db(item)
+        self.connection.commit()
         return item
