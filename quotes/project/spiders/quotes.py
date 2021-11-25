7fcb872e129beb01330f641f03f9fbba4f151432
matheusafonsouza <matheusafonsouza@gmail.com>
2021-11-24 22:18:51 -0300

    feat: using pagination

--- quotes/project/spiders/quotes.py
+++ quotes/project/spiders/quotes.py
@@ -4,11 +4,12 @@ from project.items import QuoteItem
 
 class QuoteSpider(scrapy.Spider):
     name = 'quotes'
-    start_urls = ['https://quotes.toscrape.com/']
+    base_url = 'https://quotes.toscrape.com/page/'
+    page_number = 2
+    start_urls = ['https://quotes.toscrape.com/page/1/']
 
     def parse(self, response, **kwargs):
         quotes = response.css('div.quote')
-
 
         for quote in quotes:
             title = quote.css('span.text::text').extract_first()
@@ -19,3 +20,8 @@ class QuoteSpider(scrapy.Spider):
                 author=author,
                 tags=tags
             )
+
+        next_page = self.base_url + str(self.page_number) + '/'
+        if self.page_number < 11:
+            self.page_number += 1
+            yield response.follow(next_page, callback=self.parse)
